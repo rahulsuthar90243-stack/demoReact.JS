c6f31268e59d20caf97f95bc53f692ce80a40895
Rahul <rahulsuthar90243@gmail.com>
2026-04-01 23:33:01 +0530

    folder demo use for react practice

---
+++ 01-react/src/demo.jsx
@@ -0,0 +1,10 @@
+function Demo() {
+    return (
+        <>
+            <h1>demo react project</h1>
+            <p>This is a simple demo project for learning React.</p>
+        </>
+    );
+}
+
+export default Demo;
